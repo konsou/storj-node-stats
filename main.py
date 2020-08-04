904f8016a566be4ec727940ce994e6ce00deb521
Tomi Javanainen <tomi.javanainen@gmail.com>
2020-08-04 21:15:16 +0300

    Add info about vetting progress

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from typing import List, Dict
 
 NODE_ADDRESS = 'http://192.168.42.8:14002'
+AUDIT_VETTING_TRESHOLD = 100
 
 
 def get_satellites() -> List[Dict]:
@@ -16,10 +17,18 @@ def get_satellite_info(satellite_id: str) -> Dict:
 
 if __name__ == '__main__':
     satellites = get_satellites()
+    print(f"Stats for {NODE_ADDRESS}\n")
     for satellite in satellites:
         print(satellite['url'])
         audits = get_satellite_info(satellite['id'])['audit']
-        print(f"Successful audits: {audits['successCount']} ({audits['successCount'] / audits['totalCount'] * 100: .2f} %)")
+        try:
+            print(f"Successful audits: {audits['successCount']} ({audits['successCount'] / audits['totalCount'] * 100:.2f} %)")
+            if audits['successCount'] > AUDIT_VETTING_TRESHOLD:
+                print(f"VETTING COMPLETE!")
+            else:
+                print(f"Vetting in progress - {audits['successCount'] / AUDIT_VETTING_TRESHOLD * 100:.0f} % complete")
+        except ZeroDivisionError:
+            print(f"No audits yet")
         print()
 
 
